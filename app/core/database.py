a0e4a0d7c3318e9d843beeebb7147b091f79b525
par4d15e <ericshen0911@163.com>
2025-11-13 23:45:27 +0800

    删除database.py中的隐私信息

--- app/core/database.py
+++ app/core/database.py
@@ -1,10 +1,5 @@
 from sqlmodel import create_engine
 
-# postgresql 远程连接配置
-# db_host = "pgm-uf6ym103x0oknpgdao.pg.rds.aliyuncs.com"
-# engine = create_engine(f"postgresql://postgres:SHenjiale0911!@{db_host}:5432/test")
-
-
 # 使用项目目录下的 SQLite 文件（相对路径 ./database.db）
 DATABASE_URL = "sqlite:///./database.db"
 engine = create_engine(
